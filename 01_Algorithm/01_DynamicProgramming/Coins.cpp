bc3c7bb631066b9084422309ff3be4a5cea9b1c5
Wu Yongbo <wilbur.k.woo@gmail.com>
2017-03-31 22:46:11 +0800

    add Dynamic Programming part

---
+++ 01_Algorithm/01_DynamicProgramming/Coins.cpp
@@ -0,0 +1,23 @@
+//
+// Created by wuyongbo on 17-3-31.
+//
+
+/**
+ * 如果我们有面值为1元、3元和5元的硬币若干枚，如何用最少的硬币凑够11元？
+ */
+
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    int coins[3] = {1, 3, 5};
+    int total = 11;
+
+    vector<int> coin_list;
+
+
+
+    return 0;
+}
