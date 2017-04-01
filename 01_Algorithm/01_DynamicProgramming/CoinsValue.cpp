9b18794a1ad1bb52628b4266c1db0eda2f6c839d
Wu Yongbo <wilbur.k.woo@gmail.com>
2017-04-01 21:42:27 +0800

    添加动态规划练习题

--- 01_Algorithm/01_DynamicProgramming/CoinsValue.cpp
+++ 01_Algorithm/01_DynamicProgramming/CoinsValue.cpp
@@ -1,26 +1,31 @@
-//
-// Created by wuyongbo on 17-3-31.
-//
+
+#include "CoinsValue.h"
 
 /**
  * 如果我们有面值为1元、3元和5元的硬币若干枚，如何用最少的硬币凑够11元？
  */
 
 #include <vector>
-#include <math>
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char **argv) {
+CoinsValue::CoinsValue() {
+}
+
+void CoinsValue::calculate() {
+
+    cout <<"如果我们有面值为1元、3元和5元的硬币若干枚，如何用最少的硬币凑够11元？" <<endl;
+    cout <<"\t计算结果如下：" <<endl;
 
     int coins[3] = {1, 3, 5};
     int total = 11;
     int d[12] = {0};  //存放
 
-    for (int i=1; i<11; i++) {
+    for (int i=1; i<12; i++) {
         int values[3] = {-1};
         d[i] = 1000000;
-        for (int j=0; j<2; j++) {
+        for (int j=0; j<3; j++) {
             int v = coins[j];
             if (v <= i) {
                 values[j] = d[i - v] + 1;
@@ -29,11 +34,6 @@ int main(int argc, char **argv) {
                 }
             }
         }
+        cout <<"\td[" <<i << "]=" <<"\t" << d[i] <<endl;
     }
-
-    vector<int> coin_list;
-
-
-
-    return 0;
 }
